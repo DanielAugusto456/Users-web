65cc9ba834d7f8e438eee64b84311c0ea68cf356
Daniel A <martinezapata809@gmail.com>
2026-04-02 16:59:51 -0400

    integración del archivo login.js

---
+++ Js/login.js
@@ -0,0 +1,5 @@
+const txt_nombre = document.getElementById("txt_nombre");
+const txt_contrasena = document.getElementById("txt_contrasena");
+const btn_login = document.getElementById("btn_iniciar");
+const btn_registrar = document.getElementById("btn_registrar");
+
